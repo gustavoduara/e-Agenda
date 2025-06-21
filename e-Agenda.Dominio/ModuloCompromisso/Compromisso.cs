08bbf9dab559b6181ed8d3ddeedc96af980ec58b
gabrielvsantoss <gabrielvs3131@gmail.com>
2025-06-21 15:48:37 -0300

    Realizado a criação e a montagem dos arquivos compromisso e IRepositorioCompromisso

---
+++ e-Agenda.Dominio/ModuloCompromisso/Compromisso.cs
@@ -0,0 +1,47 @@
+﻿
+
+using e_Agenda.Dominio.Compartilhado;
+
+namespace e_Agenda.Dominio.ModuloCompromisso
+{
+    public class Compromisso : EntidadeBase<Compromisso>
+    {
+        public string Assunto { get; set; }
+        public DateTime DataOcorrencia { get; set; }
+        public DateTime HoraInicio { get; set; }
+        public DateTime HoraTermino { get; set; }
+        public string TipoCompromisso  { get; set; }
+        public string Local { get; set; }
+        public string Link { get; set; }
+        public string Contato { get; set; }
+
+        public Compromisso() { }
+
+        public Compromisso(string assunto, DateTime dataOcorrencia, DateTime horaInicio, DateTime horaTermino, string tipoCompromisso, string local, string link, string contato) : this()
+        {
+            Id = Guid.NewGuid();
+            Assunto = assunto;
+            DataOcorrencia = dataOcorrencia;
+            HoraInicio = horaInicio;
+            HoraTermino = horaTermino;
+            TipoCompromisso = tipoCompromisso;
+            Local = local;
+            Link = link;
+            Contato = contato;
+
+        }
+
+        public override void AtualizarRegistro(Compromisso registroEditado)
+        {
+            Assunto = registroEditado.Assunto;
+            DataOcorrencia = registroEditado.DataOcorrencia;
+            HoraInicio = registroEditado.HoraInicio;
+            HoraTermino = registroEditado.HoraTermino;
+            TipoCompromisso = registroEditado.TipoCompromisso;
+            Local = registroEditado.Local;
+            Link = registroEditado.Link;
+            Contato = registroEditado.Contato;
+        }
+
+    }
+}
